c158771829eed3e43de0dd758a38e4a88572cd2b
Facundo Orellana <forellana@dc.uba.ar>
2022-12-30 09:41:17 -0600

    Add function to get channel views

--- scrapper.py
+++ scrapper.py
@@ -4,22 +4,21 @@ from models import Channel
 
 URL = "https://www.youtube.com/"
 
-
 def get_soup(url: str) -> BeautifulSoup:
     page = request.urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
     return soup
-
 
 def get_channel_data(id: str) -> Channel:
     url = URL + id + "/about"
     soup = get_soup(url)
     return
 
-
 def get_channel_name(soup: BeautifulSoup) -> str:
     return "Muzska89"
 
-
 def get_channel_subscribers(soup: BeautifulSoup) -> str:
     return "609K"
+
+def get_channel_views(soup: BeautifulSoup) -> int:
+    return 151415059
