1b6c5e2f9684145ec430e81dc1da39607b9c34fa
Facundo Orellana <forellana@dc.uba.ar>
2022-12-30 09:49:12 -0600

    Add test for get channel data

--- test_scrapper.py
+++ test_scrapper.py
@@ -1,5 +1,5 @@
 import pytest
-from scrapper import get_soup, get_channel_name, get_channel_subscribers, get_channel_views, get_channel_join_date
+from scrapper import get_soup, get_channel_name, get_channel_subscribers, get_channel_views, get_channel_join_date, get_channel_data
 from models import Channel
 
 
@@ -12,12 +12,11 @@ class MockChannel:
 channel = Channel(name="Muzska89", join_date="Mar 16, 2009",
                   subscribers="609K", views=151415059)
 BASE_URL = "https://www.youtube.com/"
-
+channel_id = "@Muzska89"
 
 @pytest.fixture
 def mock_channel_soup():
-    channelId = "@Muzska89"
-    url = BASE_URL + channelId + "/about"
+    url = BASE_URL + channel_id + "/about"
     return get_soup(url)
 
 
@@ -37,3 +36,7 @@ class TestGetChannel:
     def test_get_join_date(self):
         join_date = get_channel_join_date(mock_channel_soup)
         assert join_date == channel.join_date
+
+    def test_get_channel_data(self):
+        res_channel = get_channel_data(channel_id)
+        assert res_channel == channel
