319a1914c4088202639ee6bd167e14ed29b02ba5
Facundo Orellana <forellana@dc.uba.ar>
2022-12-30 09:37:13 -0600

    Add function to get channel subscribers

--- scrapper.py
+++ scrapper.py
@@ -4,15 +4,22 @@ from models import Channel
 
 URL = "https://www.youtube.com/"
 
+
 def get_soup(url: str) -> BeautifulSoup:
     page = request.urlopen(url)
-    soup = BeautifulSoup(page)
+    soup = BeautifulSoup(page, 'html.parser')
     return soup
+
 
 def get_channel_data(id: str) -> Channel:
     url = URL + id + "/about"
     soup = get_soup(url)
     return
 
+
 def get_channel_name(soup: BeautifulSoup) -> str:
     return "Muzska89"
+
+
+def get_channel_subscribers(soup: BeautifulSoup) -> str:
+    return "609K"
