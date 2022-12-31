4a092f5e2e400d5835d0608d5fc9182d44c044f4
Facundo Orellana <forellana@dc.uba.ar>
2022-12-31 08:06:44 -0600

    Add models for video and comment

--- models.py
+++ models.py
@@ -5,3 +5,14 @@ class Channel(BaseModel):
     join_date: str
     views: str
     subscribers: str
+
+class Video(BaseModel):
+    title: str
+    upload_date: str
+    views: str
+    likes: str
+
+class Comment(BaseModel):
+    user: str
+    body: str
+    likes: str
