dff53ab068d4335626b457d12943262f2df79a0c
Facundo Orellana <forellana@dc.uba.ar>
2022-12-31 08:51:03 -0600

    Add functions to scrape video data

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,6 @@
 import urllib.request as request
 from bs4 import BeautifulSoup
-from models import Channel
+from models import Channel, Video
 import json
 
 URL = "https://www.youtube.com/"
@@ -10,6 +10,11 @@ def get_soup(url: str) -> BeautifulSoup:
     page = request.urlopen(url)
     soup = BeautifulSoup(page, 'lxml')
     return soup
+
+# def saveSoup():
+#     f = open("demofile3.txt", "w", encoding='utf-8')
+#     f.write(str(soup))
+#     f.close()
 
 
 def find_key(obj, key):
@@ -84,3 +89,42 @@ def get_channel_data(id: str) -> Channel:
     channel = Channel(name=get_channel_name(soup), join_date=get_channel_join_date(soup),
                       subscribers=get_channel_subscribers(soup), views=get_channel_views(soup))
     return channel
+
+
+def get_video_script_data(soup: BeautifulSoup):
+    script = soup.find_all("script")[43].text
+    jsonLoad = json.loads(script[19:-1])
+    jsonData = find_key(jsonLoad, "videoDescriptionHeaderRenderer")
+    return jsonData
+
+
+def get_video_title(soup: BeautifulSoup) -> str:
+    script_data = get_video_script_data(soup)
+    title = script_data['title']['runs'][0]['text']
+    return title
+
+
+def get_video_date(soup: BeautifulSoup) -> str:
+    script_data = get_video_script_data(soup)
+    upload_date = script_data['publishDate']['simpleText']
+    return upload_date
+
+
+def get_video_views(soup: BeautifulSoup) -> str:
+    script_data = get_video_script_data(soup)
+    views = script_data['factoid'][1]['factoidRenderer']['value']['simpleText']
+    return views
+
+
+def get_video_likes(soup: BeautifulSoup) -> str:
+    script_data = get_video_script_data(soup)
+    views = script_data['factoid'][0]['factoidRenderer']['value']['simpleText']
+    return views
+
+
+def get_video_data(id: str) -> Video:
+    url = URL + "watch?v=" + id
+    soup = get_soup(url)
+    video = Video(title=get_video_title(soup), upload_date=get_video_date(
+        soup), views=get_video_views(soup), likes=get_video_likes(soup))
+    return video
