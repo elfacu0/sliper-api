5c4bce3d859a8fe124ea4f027cb3047e8215b3c7
Facundo Orellana <forellana@dc.uba.ar>
2023-01-04 12:29:03 -0600

    Add test for get channel avatar

--- test_scrapper_channel.py
+++ test_scrapper_channel.py
@@ -1,6 +1,6 @@
 import pytest
 import urllib.request as request
-from scrapper import get_soup, get_channel_name, get_channel_subscribers, get_channel_views, get_channel_join_date, get_channel_data
+from scrapper import get_soup, get_channel_name, get_channel_subscribers, get_channel_views, get_channel_join_date, get_channel_data, get_channel_avatar
 from models import Channel
 
 
@@ -10,6 +10,7 @@ def mock_urlopen(url: str):
 
 channel = Channel(name="Muzska89", join_date="16 mar 2009",
                   subscribers="609,000", views="151,423,488")
+avatar = "https://yt3.googleusercontent.com/ytc/AMLnZu-zAP4lxzap20jm-b81Q2aOiXEygPMdFU9n_-gKdQ=s176-c-k-c0x00ffffff-no-rj"
 BASE_URL = "https://www.youtube.com/"
 channel_id = "@Muzska89"
 
@@ -44,3 +45,8 @@ class TestGetChannel:
         monkeypatch.setattr(request, "urlopen", mock_urlopen)
         res_channel = get_channel_data(channel_id)
         assert res_channel == channel
+
+    def test_get_channel_avatar(self, monkeypatch):
+        monkeypatch.setattr(request, "urlopen", mock_urlopen)
+        res_avatar = get_channel_avatar(mock_channel_soup())
+        assert res_avatar == avatar
